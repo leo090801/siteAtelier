6854075740b571f4c892cff04b17be80cabd95a1
Léo Mory <leo.mory@divtec.ch>
2024-03-04 10:14:16 +0100

    mon site est importé

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function changeEtatMenu() {
+    let x = document.getElementById("menu-defilant");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
